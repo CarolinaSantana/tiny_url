fdfeccb59d3041218b5d98b7760157d3802e42d2
Your Name <you@example.com>
2016-04-14 18:23:00 +0000

    Finished prototype more or less

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  resources :links
+  resources :urls
+
+  root 'urls#new'
+
+  get ':identifier' => 'links#alter_to', as: 'short_link'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
